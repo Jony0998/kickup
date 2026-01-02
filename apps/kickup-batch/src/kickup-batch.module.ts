5d702e4ce012685d35ac42edbc03aba71a77ef01
Jony0998 <bozorovjanobiddin9818@gmail.com>
2026-01-02 18:24:03 +0900

    feat: integration GraphQLModule and build first GraphQL api

--- apps/kickup-batch/src/kickup-batch.module.ts
+++ apps/kickup-batch/src/kickup-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { KickupBatchController } from './kickup-batch.controller';
 import { KickupBatchService } from './kickup-batch.service';
+import {ConfigModule} from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [KickupBatchController],
   providers: [KickupBatchService],
 })
