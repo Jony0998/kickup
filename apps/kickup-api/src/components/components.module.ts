04b8018808cb327c928bdc28e9d793670ac28bca
Jony0998 <bozorovjanobiddin9818@gmail.com>
2026-01-06 21:28:44 +0900

    chore: integrate all modules into components module

--- apps/kickup-api/src/components/components.module.ts
+++ apps/kickup-api/src/components/components.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
 import { PropertyModule } from './property/property.module';
+import { MatchModule } from './match/match.module';
+import { ReviewModule } from './review/review.module';
+import { BookingModule } from './booking/booking.module';
 
 @Module({
-  imports: [MemberModule, PropertyModule]
+	imports: [
+		MemberModule,
+		PropertyModule,
+		MatchModule,
+		ReviewModule,
+		BookingModule,
+	],
 })
 export class ComponentsModule {}
